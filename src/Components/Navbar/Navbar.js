3322eb1a7f140c01c46c897e67f81560a7d1567c
Zamzam <Zamzamhassan44@gmail.com>
2022-09-09 02:20:29 +0300

    created funtion for navbar and styled it

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Navbar.css"
+import { Link } from "react-router-dom";
+
+
+function NavBar() {
+    return(
+        <div className="navbar">
+            <span className="nav-logo">Novellic</span>
+            <div className="nav-items">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/reviews">Add Book</Link>
+                <Link to="/books">Reviews</Link>
+                <Link to="/add">Add Reviews</Link>
+                <Link to="/contact">Contact Us</Link>
+            </div>
+            <div className="nav-toggle">
+                <div className="bar"></div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
