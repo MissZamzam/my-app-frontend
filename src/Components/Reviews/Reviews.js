39a3a5737241a2a9d3f91b1a1788234ea5053fba
Zamzam <Zamzamhassan44@gmail.com>
2022-09-09 02:30:53 +0300

    added functionalities of review file and styled

---
+++ src/Components/Reviews/Reviews.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react'
+import './Reviews.css'
+// import {FaTrash, FaEdit} from "react-icons/fa"
+
+function Reviews(){
+    const [comments, setComments] =useState([])
+    const[formData, setFormData] = useState({
+        name:"",
+        author:"", 
+    })
+    useEffect(()=>{
+        fetch("")
+        .then((res)=> res.json())
+        .then((data) =>{
+         setComments(data)
+        },[comments,setComments])
+        
+    });
+
+    const allComments = comments.map((comment) =>{
+        return (
+           <div key={comment.id} className="displayComment">
+                 <p >Name:  {comment.name}</p>
+                 <p >Author:  {comment.author}</p>
+                 <button>Delete</button>
+                 <button>Edit</button>
+                 
+           </div>
+           
+        )
+    })
+
+
+    //Handle Change and submit
+    function handleCommentChange(e){
+        setFormData({
+            ...formData, [e.target.name]:e.target.value
+          });
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+        const createdComment ={
+          name:formData.name,
+          author:formData.author
+        };
+    
+        fetch("",{
+          method: "POST",
+          headers:{
+            "Content-Type": "application/json",
+          },
+          body:JSON.stringify(createdComment),
+        })
+        .then(res => res.json())
+        .then(newp =>{
+            const newComments =  [...comments, newp]
+            setComments(newComments);
+            setFormData({
+              ...formData,
+              name:"",
+              author:""
+            })
+        })
+      }
+      
+    return(
+
+     <div className="commentForm">
+         <h1>Books</h1>
+         <h1>Add Book</h1>
+         <form className="updateForm">
+         <textarea value={formData.name} placeholder="Name" name="name" onChange={handleCommentChange} rows="4" ></textarea><br/>
+         <input value={formData.author} placeholder="Author" name="author"  onChange={handleCommentChange}></input><br/>
+         <input type="submit"className="submit"  onClick={handleSubmit}/>
+         </form>
+         <hr></hr>
+         <div className='res'>
+         {allComments}
+         </div>
+     </div>
+     )
+}
+export default Reviews
